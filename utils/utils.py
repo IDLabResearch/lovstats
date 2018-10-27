e13785e74d92bce224a921ab4c3d8e4036f62319
Sven Lieber <sven.lieber@ugent.be>
2018-10-27 14:35:46 +0200

    Added new helper functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 
 # -----------------------------------------------------------------------------
 def compute_depths(graph, roots):
@@ -50,3 +51,72 @@ def count_depth(graph, node, level, depth):
             depth = count_depth(graph, child, level, depth)
             level -+ 1
     return depth
+
+# -----------------------------------------------------------------------------
+def create_list_from_linked_list(graph, roots):
+    """
+    Recreate a list based on a serialized linked list
+    :param graph:
+    :param roots:
+    :return:
+
+    >>> linked_list = {"thirdEntry": "fourthEntry", "firstEntry": "secondEntry", "root": "firstEntry", "secondEntry": "thirdEntry"}
+    >>> create_list_from_linked_list(linked_list, ["root"])
+    {'root': ['firstEntry', 'secondEntry', 'thirdEntry', 'fourthEntry']}
+
+    >>> linked_list = {"root2": "firstEntry2", "firstEntry": "secondEntry", "root": "firstEntry", "secondEntry": "thirdEntry"}
+    >>> create_list_from_linked_list(linked_list, ["root", "root2"])
+    {'root': ['firstEntry', 'secondEntry', 'thirdEntry'], 'root2': ['firstEntry2']}
+    """
+    lists = {}
+    for root in roots:
+        if root in graph:
+            list_entries = find_childs(graph, root, [])
+            lists[root] = list_entries
+    return lists
+
+# -----------------------------------------------------------------------------
+def get_list_values(list, list_values, root_entries):
+    """
+
+    :param list:
+    :param list_entries:
+    :return:
+
+    >>> my_roots = {'root': 'list1', 'root2': 'list2'}
+    >>> my_list = {'root': ['firstEntry', 'secondEntry', 'thirdEntry'], 'root2': ['firstEntry2']}
+    >>> my_list_values = {'firstEntry': 'firstValue', 'secondEntry': 'secondValue', 'thirdEntry': 'thirdValue', 'firstEntry2': 'firstValue2'}
+    >>> get_list_values(my_list, my_list_values, my_roots)
+    {'list1': ['firstValue', 'secondValue', 'thirdValue'], 'list2': ['firstValue2']}
+    """
+    result_lists = {}
+    for list_root in list:
+
+        current_entries = []
+        # collect all the list entries
+        for list_entry in list[list_root]:
+            if list_entry in list_values:
+                current_entries.append(list_values[list_entry])
+
+        # Append the current entries to the result
+        if list_root in root_entries:
+            name_root = root_entries[list_root]
+        else:
+            name_root = list_root
+        result_lists[name_root] = current_entries
+    return result_lists
+
+# -----------------------------------------------------------------------------
+def find_childs(graph, parent, found_elements):
+    """
+    Recursively iterate through graph to find all list elements
+
+    >>> linked_list = {"thirdEntry": "fourthEntry", "firstEntry": "secondEntry", "root": "firstEntry", "secondEntry": "thirdEntry"}
+    >>> results = []
+    >>> find_childs(linked_list, "root", results)
+    ['firstEntry', 'secondEntry', 'thirdEntry', 'fourthEntry']
+    """
+    if parent in graph:
+        found_elements.append(graph[parent])
+        find_childs(graph, graph[parent], found_elements)
+    return found_elements
